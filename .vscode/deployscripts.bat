a58c21a574c8fdedb10a63e45653a9f7dfe99092
WindowsVistaisCool <kyle@quackings.com>
2025-07-24 01:30:36 -0400

    modify task names

--- .vscode/deployscripts.bat
+++ .vscode/deployscripts.bat
@@ -1,3 +1,3 @@
-ssh kiosk@testbench "rm -rf ~/temp && mkdir ~/temp"
+@REM ssh kiosk@testbench "rm -rf ~/temp && mkdir ~/temp"
 scp -r ./scripts kiosk@testbench:/home/kiosk/
 ssh kiosk@testbench "cd ~/scripts && chmod +x *.sh && ./install.sh"
