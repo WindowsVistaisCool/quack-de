618a65456b39049bc64a03f587359139c2d973e3
WindowsVistaisCool <kyle@quackings.com>
2025-09-04 10:41:11 -0400

    reorder

--- src/pages/LEDs.py
+++ src/pages/LEDs.py
@@ -38,9 +38,9 @@ class LEDsPage(NavigationPage):
         # goes (0, 0), (0, 1), (1, 0), (1, 1), etc. in grid units
         arrangement = (
             LEDThemes.getTheme("twinkle"),
+            LEDThemes.getTheme("pacifica"),
             LEDThemes.getTheme("rgbSnake"),
             LEDThemes.getTheme("fire2012"),
-            LEDThemes.getTheme("pacifica"),
             LEDThemes.getTheme("rainbow"),
             # LEDThemes.getTheme("epilepsy"),
             # LEDThemes.getTheme("ledSelector"),
